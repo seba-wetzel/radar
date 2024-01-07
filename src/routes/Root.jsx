e238468a31b25e0ce99dbb22700cd0a998f50888
Niv Sardi <xaiki@evilgiggle.com>
2024-01-07 06:42:12 -0300

    routes: indent Root.jsx

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,15 +2,16 @@
 import { Outlet } from "react-router-dom";
 import Marquee from '../components/marquee.jsx';
 import NavbarBootstrap from "../components/navbar.jsx";
+import Footer from "../components/footer.jsx"
 
 
 export default function Root() {
-    return (
-        <>
-
-<NavbarBootstrap /> {/* Navbar siempre se muestra */}
-<Marquee/>
-<Outlet/>
-</>
-  );
-}                        <Footer />
+        return (
+                <>
+                        <NavbarBootstrap /> {/* Navbar siempre se muestra */}
+                        <Marquee/>
+                        <Outlet/>
+                        <Footer />
+                </>
+        );
+}
