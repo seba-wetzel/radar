46bf3169b03c2e77235f4da30159b2091acd2349
Niv Sardi <xaiki@evilgiggle.com>
2024-01-08 09:52:33 -0300

    performance: vite.config disable sourcemap and spilt
    disable sourcemaps
    manually split components
    explicitly request cssCodeSplit
    invalidate module dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,27 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+    plugins: [react()],
+    base: './',
+    build: {
+        outDir: 'build',
+        sourcemap: false,
+        modulePreload: {
+            resolveDependencies: (url, deps, context) => {
+                return [];
+            },
+        },
+        cssCodeSplit: true,
+        rollupOptions: {
+                        output: {
+                                 sourcemap: false,
+                                 manualChunks: {
+                                                router: ['react-router-dom'],
+                                                rmapgl: ['react-map-gl'],
+                                                mapgl: ['maplibre-gl'],
+                                                bootstrap: ['react-bootstrap']
+                                                },
+                                 },
+                        },
+    },
 })
